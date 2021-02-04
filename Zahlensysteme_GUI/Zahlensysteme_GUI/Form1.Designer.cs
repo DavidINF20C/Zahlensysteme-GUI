44d63b58a6d4739f7619902308b0168fa66224a2
DavidINF20C <75478117+DavidINF20C@users.noreply.github.com>
2021-02-04 19:31:32 +0100

    Add files via upload

---
+++ Zahlensysteme_GUI/Zahlensysteme_GUI/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace Zahlensysteme_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comBoxUrsprung = new System.Windows.Forms.ComboBox();
+            this.comBoxZiel = new System.Windows.Forms.ComboBox();
+            this.txtAusgangszahl = new System.Windows.Forms.TextBox();
+            this.btnBerechnen = new System.Windows.Forms.Button();
+            this.txtResultat = new System.Windows.Forms.TextBox();
+            this.lblUrsprung = new System.Windows.Forms.Label();
+            this.lblAusgang = new System.Windows.Forms.Label();
+            this.lblZiel = new System.Windows.Forms.Label();
+            this.lboxError = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // comBoxUrsprung
+            // 
+            this.comBoxUrsprung.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comBoxUrsprung.FormattingEnabled = true;
+            this.comBoxUrsprung.Location = new System.Drawing.Point(41, 105);
+            this.comBoxUrsprung.Name = "comBoxUrsprung";
+            this.comBoxUrsprung.Size = new System.Drawing.Size(121, 21);
+            this.comBoxUrsprung.TabIndex = 0;
+            this.comBoxUrsprung.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comBoxZiel
+            // 
+            this.comBoxZiel.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comBoxZiel.FormattingEnabled = true;
+            this.comBoxZiel.Location = new System.Drawing.Point(41, 254);
+            this.comBoxZiel.Name = "comBoxZiel";
+            this.comBoxZiel.Size = new System.Drawing.Size(121, 21);
+            this.comBoxZiel.TabIndex = 1;
+            // 
+            // txtAusgangszahl
+            // 
+            this.txtAusgangszahl.Location = new System.Drawing.Point(352, 106);
+            this.txtAusgangszahl.Name = "txtAusgangszahl";
+            this.txtAusgangszahl.Size = new System.Drawing.Size(275, 20);
+            this.txtAusgangszahl.TabIndex = 2;
+            // 
+            // btnBerechnen
+            // 
+            this.btnBerechnen.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBerechnen.Location = new System.Drawing.Point(397, 231);
+            this.btnBerechnen.Name = "btnBerechnen";
+            this.btnBerechnen.Size = new System.Drawing.Size(75, 23);
+            this.btnBerechnen.TabIndex = 3;
+            this.btnBerechnen.Text = "Berechnen !";
+            this.btnBerechnen.UseVisualStyleBackColor = true;
+            this.btnBerechnen.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtResultat
+            // 
+            this.txtResultat.Location = new System.Drawing.Point(352, 260);
+            this.txtResultat.Name = "txtResultat";
+            this.txtResultat.Size = new System.Drawing.Size(171, 20);
+            this.txtResultat.TabIndex = 4;
+            this.txtResultat.Text = "Hier wird das Resultat ausgegeben.";
+            this.txtResultat.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // lblUrsprung
+            // 
+            this.lblUrsprung.AutoSize = true;
+            this.lblUrsprung.Location = new System.Drawing.Point(41, 68);
+            this.lblUrsprung.Name = "lblUrsprung";
+            this.lblUrsprung.Size = new System.Drawing.Size(184, 13);
+            this.lblUrsprung.TabIndex = 5;
+            this.lblUrsprung.Text = "Bitte Ursprungs-Zahlensystem wählen";
+            // 
+            // lblAusgang
+            // 
+            this.lblAusgang.AutoSize = true;
+            this.lblAusgang.Location = new System.Drawing.Point(349, 68);
+            this.lblAusgang.Name = "lblAusgang";
+            this.lblAusgang.Size = new System.Drawing.Size(278, 13);
+            this.lblAusgang.TabIndex = 6;
+            this.lblAusgang.Text = "Bitte geben Sie die Zahl ein die umberechnet werden soll.";
+            // 
+            // lblZiel
+            // 
+            this.lblZiel.AutoSize = true;
+            this.lblZiel.Location = new System.Drawing.Point(38, 217);
+            this.lblZiel.Name = "lblZiel";
+            this.lblZiel.Size = new System.Drawing.Size(153, 13);
+            this.lblZiel.TabIndex = 7;
+            this.lblZiel.Text = "Bitte Ziel-Zahlensystem wählen";
+            // 
+            // lboxError
+            // 
+            this.lboxError.FormattingEnabled = true;
+            this.lboxError.Location = new System.Drawing.Point(41, 343);
+            this.lboxError.Name = "lboxError";
+            this.lboxError.Size = new System.Drawing.Size(747, 95);
+            this.lboxError.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lboxError);
+            this.Controls.Add(this.lblZiel);
+            this.Controls.Add(this.lblAusgang);
+            this.Controls.Add(this.lblUrsprung);
+            this.Controls.Add(this.txtResultat);
+            this.Controls.Add(this.btnBerechnen);
+            this.Controls.Add(this.txtAusgangszahl);
+            this.Controls.Add(this.comBoxZiel);
+            this.Controls.Add(this.comBoxUrsprung);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comBoxUrsprung;
+        private System.Windows.Forms.ComboBox comBoxZiel;
+        private System.Windows.Forms.TextBox txtAusgangszahl;
+        private System.Windows.Forms.Button btnBerechnen;
+        private System.Windows.Forms.TextBox txtResultat;
+        private System.Windows.Forms.Label lblUrsprung;
+        private System.Windows.Forms.Label lblAusgang;
+        private System.Windows.Forms.Label lblZiel;
+        private System.Windows.Forms.ListBox lboxError;
+    }
+}
+
---
+++ Zahlensysteme_GUI/Zahlensysteme_GUI/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zahlensysteme_GUI
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Diese Datenbank ist für die Zahlensysteme die umgerechnet werden.
+        /// </summary>
+        DataTable UrsprungsZahlenSystem = new DataTable();
+
+        /// <summary>
+        /// Diese Datenbank ist für die Ziel berechnung zuständig.
+        /// </summary>
+        DataTable ZielZahlenSystem = new DataTable();
+
+        /// <summary>
+        /// Diese Datenbank ist dafür zuständig, dass die richtigen Daten bereitgestellt werden.
+        /// </summary>
+        DataTable ZielZahlenSystemAuswahl = new DataTable();
+
+        public static string gewählteAusgangsZahl; //
+        public static bool nummernTest; //Boolean benötigt zur verifizierung, dass die Zahl umgerechnet werden kann
+        public static int errorHochzähler;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lboxError.Hide();
+            FülleUrsprungsZahlenSystem();
+            FülleZielZahlenSystem();
+            comBoxUrsprung.DataSource = UrsprungsZahlenSystem;
+            comBoxUrsprung.DisplayMember = "UZSystemName";
+        }
+
+        /// <summary>
+        /// Mit dieser Methode wird eine Datenbank erstellt, welche die Daten für das Ursprungs-Zahlensystem bereitstellt.
+        /// </summary>
+        private void FülleUrsprungsZahlenSystem()
+        {
+            UrsprungsZahlenSystem.Columns.Add("UZSystemID", typeof(int));
+            UrsprungsZahlenSystem.Columns.Add("UZSystemName");
+            UrsprungsZahlenSystem.Columns.Add("UZSystemWert", typeof(int));
+            UrsprungsZahlenSystem.Columns.Add("UZSystemChar", typeof(char));
+
+
+            UrsprungsZahlenSystem.Rows.Add(1, "Binär", 2, '1');
+            UrsprungsZahlenSystem.Rows.Add(2, "Oktal", 8, '7');
+            UrsprungsZahlenSystem.Rows.Add(3, "Dezimal", 10, '9');
+            UrsprungsZahlenSystem.Rows.Add(4, "Hexadezimal", 16, 'F');
+        }
+        /// <summary>
+        /// Mit dieser Methode werden die Daten für das Ziel-System bereitgestellt. Dabei können nur kompatible Systeme ausgewählt werden.
+        /// </summary>
+        private void FülleZielZahlenSystem()
+        {
+            ZielZahlenSystem.Columns.Add("UZSystemID", typeof(int));
+            ZielZahlenSystem.Columns.Add("ZZSystemName");
+            ZielZahlenSystem.Columns.Add("ZZSystemWert", typeof(int));
+
+            ZielZahlenSystem.Rows.Add(1, "Oktal", 8);
+            ZielZahlenSystem.Rows.Add(1, "Dezimal", 10);
+            ZielZahlenSystem.Rows.Add(1, "Hexadezimal", 16);
+
+            ZielZahlenSystem.Rows.Add(2, "Binär", 2);
+            ZielZahlenSystem.Rows.Add(2, "Dezimal", 10);
+            ZielZahlenSystem.Rows.Add(2, "Hexadezimal", 16);
+
+            ZielZahlenSystem.Rows.Add(3, "Binär", 2);
+            ZielZahlenSystem.Rows.Add(3, "Oktal", 8);
+            ZielZahlenSystem.Rows.Add(3, "Hexadezimal", 16);
+
+            ZielZahlenSystem.Rows.Add(4, "Binär", 2);
+            ZielZahlenSystem.Rows.Add(4, "Oktal", 8);
+            ZielZahlenSystem.Rows.Add(4, "Dezimal", 10);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Hierbei wird genau die Datenbank-Zeile für das Ziel-System erstellt.
+            ZielZahlenSystemAuswahl = ZielZahlenSystem.Select("UZSystemID=" + UrsprungsZahlenSystem.Rows[comBoxUrsprung.SelectedIndex]["UZSystemID"]).CopyToDataTable();
+
+            comBoxZiel.DataSource = ZielZahlenSystemAuswahl;
+            comBoxZiel.DisplayMember = "ZZSystemName";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            long zwischenErgebnis;
+            int gewähltesUrsprungsZahlenSystem = Convert.ToInt32(UrsprungsZahlenSystem.Rows[comBoxUrsprung.SelectedIndex]["UZSystemWert"]);
+            int gewähltesZielZahlenSystem = Convert.ToInt32(ZielZahlenSystemAuswahl.Rows[comBoxZiel.SelectedIndex]["ZZSystemWert"]);
+            string ergebnis;
+
+            try
+            {
+                gewählteAusgangsZahl = txtAusgangszahl.Text;
+                nummernTest = NummernTest(gewählteAusgangsZahl, Convert.ToChar(UrsprungsZahlenSystem.Rows[comBoxUrsprung.SelectedIndex]["UZSystemChar"]));
+
+                if (nummernTest == false)
+                {
+                    MessageBox.Show("Diese Zahl kann nicht umgerechnet werden!");
+                    txtAusgangszahl.Text = "";
+                    txtResultat.Text = "";
+                }
+                else
+                {
+                    if (gewähltesZielZahlenSystem == 10)//Falls eine Berechnung ins Dezimalsystem durchgeführt wird
+                    {
+                        zwischenErgebnis = Convert.ToInt64(gewählteAusgangsZahl, gewähltesUrsprungsZahlenSystem);
+                        ergebnis = Convert.ToString(zwischenErgebnis);
+                    }
+                    else if (gewähltesUrsprungsZahlenSystem == 10)//Falls eine Berechnung vom Dezimalsystem durchgeführt wird
+                    {
+                        zwischenErgebnis = Convert.ToInt64(DivisionsRest(gewählteAusgangsZahl, gewähltesZielZahlenSystem));
+                        ergebnis = Convert.ToString(zwischenErgebnis);
+                    }
+                    else //Für die Berechnung aller anderen Zahlensysteme
+                    {
+                        zwischenErgebnis = Convert.ToInt64(gewählteAusgangsZahl, gewähltesUrsprungsZahlenSystem);
+                        ergebnis = Convert.ToString(zwischenErgebnis);
+                        ergebnis = DivisionsRest(ergebnis, gewähltesZielZahlenSystem);
+                    }
+                    txtResultat.Text = ergebnis;
+                }
+            }
+            catch (ArgumentOutOfRangeException er)
+            {
+                errorHochzähler++;
+                lboxError.Show();
+                MessageBox.Show("Gib eine Nummer ein!");
+                lboxError.Items.Add($"Error Nummer {errorHochzähler}: { er.Message}");
+
+            }
+            catch (Exception ex)
+            {
+                errorHochzähler++;
+                lboxError.Show();
+                MessageBox.Show(ex.Message);
+                lboxError.Items.Add($"Error Nummer {errorHochzähler}: { ex.Message}");
+            }
+        }
+        public static bool NummernTest(string ausgangsZahl, char ursprungsZSystem)
+        {
+            char[] ausgangArray;
+            bool Nummerntest = true;
+            ausgangsZahl = ausgangsZahl.ToUpper();
+
+            ausgangArray = ausgangsZahl.ToCharArray();
+
+            for (int i = 0; i < ausgangsZahl.Length; i++)
+            {
+                if (ausgangArray[i] > ursprungsZSystem)
+                {
+                    Nummerntest = false;
+                }
+            }
+            return Nummerntest;
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        public static string DivisionsRest(string ausgangsZahl, int zielZahlensystem)
+        {
+            long rest;
+            long zahlNeu = 0;
+            string ergebnis2 = "";
+            string restString = "";
+            long ausgangsInt;
+            ausgangsInt = Convert.ToInt64(ausgangsZahl);
+
+            do
+            {
+                try
+                {
+                    rest = ausgangsInt % zielZahlensystem;
+                    if (rest > 9)
+                    {
+                        restString = Convert.ToString(Convert.ToChar(rest + 55));
+                        ergebnis2 = Convert.ToString(restString + ergebnis2);
+                    }
+                    else if (rest <= 9)
+                    {
+                        ergebnis2 = Convert.ToString(rest + ergebnis2);
+                    }
+                    zahlNeu = ausgangsInt / zielZahlensystem;
+                    ausgangsInt = zahlNeu;
+                }
+                catch (Exception e)
+                {
+                    zahlNeu = 0;
+                    MessageBox.Show(e.Message);
+                }
+
+            } while (zahlNeu != 0);
+            return ergebnis2;
+        }
+    }
+}
